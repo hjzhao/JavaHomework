5fc19ef9563e670eb236356b22cb92ff50fc2db3
zhaohaijun <zhao.haijun@immomo.com>
2021-05-14 10:45:21 +0800

    week07

---
+++ week07/abstractrouting/src/main/java/com/zhj/abstractrouting/AbstractroutingApplication.java
@@ -0,0 +1,14 @@
+package com.zhj.abstractrouting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbstractroutingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbstractroutingApplication.class, args);
+		System.out.println("hellowrold");
+	}
+
+}
---
+++ week07/abstractrouting/src/test/java/com/zhj/abstractrouting/AbstractroutingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhj.abstractrouting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbstractroutingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
